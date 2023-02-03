10bfc17f20808ead0c12b14fb707562ad99feb29
Ajay Singh <90884963+phhgsi@users.noreply.github.com>
2023-02-03 17:08:43 +0530

    Create ContactUs.js

---
+++ src/components/ContactUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3, 2),
+  },
+}));
+
+export default function ContactUs() {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h5" component="h
